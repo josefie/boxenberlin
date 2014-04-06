dc7428f7c8da681b6cf6365ace92f92632c8383e
lemauski <lemauski@DeepThought.(none)>
2014-04-06 18:35:33 +0200

    flash message if access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = "Sie haben auf diese Seite keinen Zugriff."
+    redirect_to root_path
+  end
+
   private
 
   def current_user
